4c4b266bc48ed5d5c58ad75a3786959b206f0fc0
Confusified <60018584+Confusified@users.noreply.github.com>
2022-09-18 17:44:53 +0200

    Update Panik.lua

--- Panik.lua
+++ Panik.lua
@@ -21,7 +21,7 @@ if game.PlaceId == 8511615377 then
     print("You are currently in the lobby, script has been queued for teleport.")
     game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
-        syn.queue_on_teleport(loadstring(game:HttpGet('https://gist.githubusercontent.com/Confusified/13da12ee14c6d8b7e5e254e8368b2c94/raw/Panik.txt')))
+        syn.queue_on_teleport(loadstring(game:HttpGet('https://raw.githubusercontent.com/Confusified/awesomenoodles/main/Panik.lua')))
     end
 end)
 end

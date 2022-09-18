c44dcb115c38ee8d023e2c1973feaa23a2c0cce5
Confusified <60018584+Confusified@users.noreply.github.com>
2022-09-18 21:36:15 +0200

    Update Panik.lua

--- Panik.lua
+++ Panik.lua
@@ -21,7 +21,7 @@ if game.PlaceId == 8511615377 then
     print("You are currently in the lobby, script has been queued for teleport.")
     game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
-        syn.queue_on_teleport(loadstring(game:HttpGet('https://raw.githubusercontent.com/Confusified/awesomenoodles/main/Panik.lua'))())
+        syn.queue_on_teleport('loadstring(game:HttpGet('https://raw.githubusercontent.com/Confusified/awesomenoodles/main/Panik.lua'))()')
     end
 end)
 return
@@ -47,7 +47,22 @@ local exittoggle = sec:Toggle("Highlight Exits",settings.ShowExit,"Toggle",funct
     writefile(fullFileName,game:GetService("HttpService"):JSONEncode(settings)) --update config
     
     if settings.ShowExit then
-        
+            for i,v in ipairs(game:GetService("Workspace"):WaitForChild("Trapdoors")) do
+                local ExitHighlight = Highlight:Clone()
+                ExitHighlight.Parent = ScriptData
+                ExitHighlight.Adornee = v
+                ExitHighlight.Name = "Exit"..i
+            end
+        print("Highlighting Exits")
+    else
+            for i,v in ipairs(game:GetService("Workspace"):WaitForChild("Trapdoors"))
+        local ExitHighlight = ScriptData:FindFirstChild("Exit"..i)
+        if ExitHighlight == nil then
+            print("Could not find highlight")
+            return
+        end
+        ExitHighlight:Destroy()
+        print("No longer highlighting Exits")
     end
 end)
 local happytoggle = sec:Toggle("Highlight Happy",settings.ShowHappy,"Toggle",function(a)

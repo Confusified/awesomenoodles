9ce6fee37ed97896c1f11235cc40aa96cd783d12
Confusified <60018584+Confusified@users.noreply.github.com>
2022-09-18 19:02:14 +0200

    Update Panik.lua

--- Panik.lua
+++ Panik.lua
@@ -9,15 +9,18 @@ local fName = "ConConfigs"
 local FileName = "Panik.txt"
 local fullFileName = fName.."\\"..FileName
 
-if not isfolder(fName) then
+if not isfolder(fName) then --locate folder
     print("Could not find configuration folder, creating a new one.")
    makefolder(fName) 
 end
-if not isfile(fullFileName) or isfile(fullFileName) and readfile(fullFileName) == "" then
+if not isfile(fullFileName) or isfile(fullFileName) and readfile(fullFileName) == "" then --locate game config
     print("Configuration file for this game is missing or broken, creating a new one.")
     writefile(fullFileName,game:GetService("HttpService"):JSONEncode(settingsTable))
 end
-if game.PlaceId == 8511615377 then
+if not game:IsLoaded() then --wait until the game is loaded
+    game.Loaded:Wait()
+end
+if game.PlaceId == 8511615377 then --if in lobby queue script
     print("You are currently in the lobby, script has been queued for teleport.")
     game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
@@ -26,5 +29,11 @@ if game.PlaceId == 8511615377 then
 end)
 end
 
-local settings = game:GetService("HttpService"):JSONDecode(readfile(fullFileName))
+local settings = game:GetService("HttpService"):JSONDecode(readfile(fullFileName)) --fetch game config
 print("Settings have been fetched. Applying now.")
+
+local Workspace = game:GetService("Workspace")
+local TrapdoorFolder = Workspace:WaitForChild("Trapdoors")
+local PillarFolder = Workspace:WaitForChild("Scanners")
+--local Happy = game:GetService("Workspace").Happy
+

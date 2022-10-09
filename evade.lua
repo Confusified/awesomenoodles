58da5d7e4c8f3aa139d1fdb20b17cf2022820b60
Confusified <60018584+Confusified@users.noreply.github.com>
2022-10-09 19:48:27 +0200

    Added file customizations
    not yet functional

--- evade.lua
+++ evade.lua
@@ -1,3 +1,27 @@
+local settingsTable = {
+    NextbotESP = true,
+		NextbotESPColor = Color3.fromRGB(255,0,0),
+    PlayerESP = true,
+		PlayerESPColor = Color3.fromRGB(0,0,255),
+		DownedESP = true,
+		DownedESPColor = Color3.fromRGB(255,155,0),
+		JumpCanBeHeld = false
+		
+    }
+local fName = "ConConfigs"
+local FileName = "Evade.txt"
+local fullFileName = fName.."\\"..FileName
+
+if not isfolder(fName) then
+    print("Could not find configuration folder, creating a new one.")
+   makefolder(fName) 
+end
+if not isfile(fullFileName) or isfile(fullFileName) and readfile(fullFileName) == "" then
+    print("Configuration file for this game is missing or broken, creating a new one.")
+    writefile(fullFileName,game:GetService("HttpService"):JSONEncode(settingsTable))
+end
+
+
 local BadgeService = game:GetService("BadgeService")
 local GameFolder = game:GetService("Workspace"):WaitForChild("Game")
 local WS_Players = GameFolder:WaitForChild("Players")
@@ -44,29 +68,32 @@ if not b:FindFirstChild("Highlight") then
 end
 end
 if not game:GetService("CoreGui"):FindFirstChild("EvadeGui") then
-local MainGui = Instance.new("ScreenGui")
-MainGui.Name = "EvadeGui"
-gethui(MainGui)
-MainGui.Parent = game:GetService("CoreGui")
-local TimeLeft = Instance.new("TextLabel",MainGui)
-TimeLeft.Name = "TimeRemaining"
-TimeLeft.BackgroundTransparency = 1
-TimeLeft.Size = UDim2.new(0.18,0,0.04,0)
-TimeLeft.Position = UDim2.new(0.5-(TimeLeft.Size.X.Scale/2),0,0.13,0)
-TimeLeft.TextScaled = true
-TimeLeft.TextColor3 = Color3.new(1,1,1)
-TimeLeft.TextStrokeTransparency = 0.9
-TimeLeft.TextStrokeColor3 = Color3.new(0,0,0)
-TimeLeft.Font = Enum.Font.GothamBold
+		local MainGui = Instance.new("ScreenGui")
+		MainGui.Name = "EvadeGui"
+		gethui(MainGui)
+		MainGui.Parent = game:GetService("CoreGui")
+	
+		local TimeLeft = Instance.new("TextLabel",MainGui)
+		TimeLeft.Name = "TimeRemaining"
+		TimeLeft.BackgroundTransparency = 1
+		TimeLeft.Size = UDim2.new(0.18,0,0.04,0)
+		TimeLeft.Position = UDim2.new(0.5-(TimeLeft.Size.X.Scale/2),0,0.13,0)
+		TimeLeft.TextScaled = true
+		TimeLeft.TextColor3 = Color3.new(1,1,1)
+		TimeLeft.TextStrokeTransparency = 0.9
+		TimeLeft.TextStrokeColor3 = Color3.new(0,0,0)
+		TimeLeft.Font = Enum.Font.GothamBold
 
-local function updateTimer()
-local maxval = math.max(0, GameStats:GetAttribute("TimeRemaining"));
-TimeLeft.Text = string.sub(string.format("%02d:%02d", math.floor(maxval / 60), math.floor(maxval) % 60), 2);
-end
-updateTimer()
-workspace.Game.Stats:GetAttributeChangedSignal("TimeRemaining"):Connect(function()
-	updateTimer()
-    end);
+		local function updateTimer()
+				local maxval = math.max(0, GameStats:GetAttribute("TimeRemaining"));
+				TimeLeft.Text = string.sub(string.format("%02d:%02d", math.floor(maxval / 60), math.floor(maxval) % 60), 2);
+		end
+	
+		updateTimer()
+	
+		workspace.Game.Stats:GetAttributeChangedSignal("TimeRemaining"):Connect(function()
+				updateTimer()
+		end);
 end
 
 local ObjectivesFolder = GameFolder:WaitForChild("Parts"):WaitForChild("Objectives")
